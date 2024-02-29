dd9b77979543c750f2df192da232b92f839d12c0
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-02-29 16:10:58 +0900

    [Bronze V] Title: A+B - 4, Time: 136 ms, Memory: 9564 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n"); //여러 줄 입력받을 때
 
 for (let i = 0; i < input.length; i++) {
-  const [a, b] = input[i].split(" ").map(Number);
+  const tmp = input[i].split(" ");
+  const a = parseInt(tmp[0]);
+  const b = parseInt(tmp[1]);
   console.log(a + b);
 }
