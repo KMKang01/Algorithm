dbea13bbca23b0678a2b9092174d9c6b78d533db
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-06-17 16:28:56 +0900

    [Bronze V] Title: Hook, Time: 116 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+        System.out.println(sb);
+    }
+}
