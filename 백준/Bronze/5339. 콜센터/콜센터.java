69eae30f7de22c159a2db1ac7a7ca053a49aabf3
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-06-21 11:19:30 +0900

    [Bronze V] Title: 콜센터, Time: 120 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+
+
+        System.out.println(sb);
+    }
+}
