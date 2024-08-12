1210a0f8fcc5e07371cd0256a63c0cf7b4abab96
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-08-13 02:06:24 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 328 ms, Memory: 20252 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N ; i++) {
+            for (int j = 0; j < N-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1 ; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
