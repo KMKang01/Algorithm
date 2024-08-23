d7339efaae252e2344cb776e7d445015e3b1bc35
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-08-24 03:56:44 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 968 ms, Memory: 144240 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static boolean[] visited;
+    static int[] list;
+    static int count = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        int N = Integer.parseInt(input.split(" ")[0]);
+        int M = Integer.parseInt(input.split(" ")[1]);
+        int R = Integer.parseInt(input.split(" ")[2]);
+
+        List<Integer>[] graph = new List[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        visited = new boolean[N + 1];
+        list = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            String line = br.readLine();
+            int a = Integer.parseInt(line.split(" ")[0]);
+            int b = Integer.parseInt(line.split(" ")[1]);
+
+            graph[a].add(b);
+            graph[b].add(a); // 무방향 == 양방향 그래프
+        }
+
+        for (List<Integer> node : graph) {
+            if (node != null) {
+                node.sort(Comparator.reverseOrder());
+            }
+        }
+
+        DFS(graph, R);
+
+        for (int i =1; i < list.length;i++) {
+            sb.append(list[i]).append("\n");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+
+    static void printGraph(List<Integer>[] graph) {
+        for (List<Integer> node : graph) {
+            if (node.isEmpty()) {
+                System.out.println("No Edges");
+                continue;
+            }
+            for (int a : node) {
+                System.out.print(a + " -> ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void DFS(List<Integer>[] graph, int r) {
+        visited[r] = true;
+        list[r] = count++;
+        for (int v : graph[r]) {
+            if (!visited[v])
+                DFS(graph, v);
+        }
+    }
+}
