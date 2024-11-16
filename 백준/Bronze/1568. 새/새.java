23ce369885d189fbfb6225a8d6616543567ad9cc
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-11-17 00:00:57 +0900

    [Bronze II] Title: 새, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/1568. 새/새.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int K = 1;
+		int second = 0;
+		while(N > 0){
+			if(N < K)
+				K=1;
+			N -= K;
+			K++;
+			second++;
+		}
+		System.out.println(second);
+	}
+}
