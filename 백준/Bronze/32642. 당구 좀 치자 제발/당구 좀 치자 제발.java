41817745daa36f0252b18ee37f0e3cbd3708cb03
kmk01 <144207120+KMKang01@users.noreply.github.com>
2025-04-25 13:08:35 +0900

    [Bronze IV] Title: 당구 좀 치자 제발, Time: 264 ms, Memory: 32048 KB -BaekjoonHub

---
+++ 백준/Bronze/32642. 당구 좀 치자 제발/당구 좀 치자 제발.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		String[] str = br.readLine().split(" ");
+		long rage = 0;
+		long rageSum = 0;
+
+		for (String s : str) {
+			if(s.equals("0")) {
+				rage--;
+				rageSum += rage;
+			} else {
+				rage++;
+				rageSum += rage;
+			}
+		}
+		System.out.println(rageSum);
+	}
+}
+
