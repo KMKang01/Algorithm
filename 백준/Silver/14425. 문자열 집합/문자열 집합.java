09b84c2bc65fdd909f74737e9a6aba195fe3a942
kmk01 <144207120+KMKang01@users.noreply.github.com>
2025-07-11 15:10:45 +0900

    [Silver IV] Title: 문자열 집합, Time: 384 ms, Memory: 31184 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,25 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
-        int count = 0;
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        sc.nextLine();
-        for (int i = 0; i < N; i++) {
-            String line = sc.nextLine();
-            list.add(line);
-        }
-        for(int i = 0; i < M; i++){
-            String line = sc.nextLine();
-            if(list.contains(line)){
-                count++;
-            }
-        }
-        System.out.println(count);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String  [] line1 = br.readLine().split(" ");
+		int N = Integer.parseInt(line1[0]);
+		int M = Integer.parseInt(line1[1]);
 
+		Set<String> set = new HashSet<String>();
 
-    }
+		for (int i = 0; i < N ; i++) {
+			set.add(br.readLine());
+		}
+
+		int count = 0;
+
+		for (int i = 0; i < M ; i++) {
+			count = set.contains(br.readLine()) ? count + 1 : count;
+		}
+		System.out.println(count);
+
+	}
 }
+
+
