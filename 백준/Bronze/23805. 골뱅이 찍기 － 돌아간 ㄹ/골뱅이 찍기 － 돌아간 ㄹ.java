01cd4e8af67fad7ce6ddda2133f4b44000d8061b
kmk01 <144207120+KMKang01@users.noreply.github.com>
2025-08-03 20:41:50 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 164 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N ; i++) {
+			for (int j = 0; j < 3 * N; j++) {
+				sb.append("@");
+			}
+			for (int j = 0; j < N; j++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+
+
+
+		for (int i = 0; i < 3*N ; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append("@");
+			}
+			for (int j = 0; j < N; j++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				sb.append("@");
+			}
+			for (int j = 0; j < N; j++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append("@");
+			}
+			for (int j = 0; j < N; j++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j < 3 * N; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+
+
+		System.out.println(sb.toString().trim());
+	}
+}
