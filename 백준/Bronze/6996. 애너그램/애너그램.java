59cfc734f064c3cb69e630aaa99cc8cfdeda0148
kmk01 <144207120+KMKang01@users.noreply.github.com>
2025-09-07 00:56:49 +0900

    [Bronze I] Title: 애너그램, Time: 196 ms, Memory: 18588 KB -BaekjoonHub

---
+++ 백준/Bronze/6996. 애너그램/애너그램.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    private static boolean solveAnagrams(String first, String second ) {
+        /* ------------------- INSERT CODE HERE --------------------
+         *
+         * Your code should return true if the two strings are anagrams of each other.
+         *
+         * */
+        if(first.length() != second.length()) return false;
+        
+        ArrayList<String> firstSplitList = new ArrayList<>();
+        for(String c: first.split("")){
+            firstSplitList.add(c);
+        }
+        
+        for(String c: second.split("")){
+            firstSplitList.remove(c);
+        }
+
+        return firstSplitList.size() == 0;
+
+        /* -------------------- END OF INSERTION --------------------*/
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numTests = sc.nextInt();
+
+        for (int i = 0; i < numTests; i++) {
+            String first = sc.next().toLowerCase();
+            String second = sc.next().toLowerCase();
+
+            System.out.println(first + " & " + second + " are " + (solveAnagrams(first, second) ? "anagrams." : "NOT anagrams."));
+        }
+    }
+}
