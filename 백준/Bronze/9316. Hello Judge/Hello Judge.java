d731432ec426cd49303f6e4cde97c2b6a36e0847
kmk01 <144207120+KMKang01@users.noreply.github.com>
2025-09-11 04:03:43 +0900

    [Bronze V] Title: Hello Judge, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= N ; i++) {
+			sb.append("Hello World, Judge ").append(i).append("!\n");
+		}
+		System.out.println(sb.toString().trim());
+	}
+
+
+}
