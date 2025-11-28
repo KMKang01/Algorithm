b3ca9bc5521fca1fe4fe40aaff3dfb4d4503795a
kmk01 <144207120+KMKang01@users.noreply.github.com>
2025-11-28 23:34:29 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.java
@@ -0,0 +1,21 @@
+  import java.io.*;
+  import java.util.*;
+
+  public class Main {
+      public static void main(String[] args) throws IOException {
+          BufferedReader br = new BufferedReader(new
+  InputStreamReader(System.in));
+
+          int N = Integer.parseInt(br.readLine());
+
+          if (200 <= N && N <= 205) {
+              System.out.println(1);
+          } else if ( 206 <= N && N  <= 217) {
+              System.out.println(2);
+          } else if ( 218 <= N && N  <= 228) {
+              System.out.println(3);
+          } else {
+              System.out.println(4);
+          }
+      }
+  }
