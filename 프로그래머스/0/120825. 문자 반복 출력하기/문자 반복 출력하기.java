c2dfcb11cabc85e832f8fab296c2b2d6ebf12f99
kmk01 <144207120+KMKang01@users.noreply.github.com>
2026-04-08 00:05:34 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.18 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(String s:my_string.split("")){
+            sb.append(s.repeat(n));
+        }
+        return sb.toString();
+    }
+}
