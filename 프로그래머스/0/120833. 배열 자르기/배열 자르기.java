24358ffa241793c17e0356a566948a06016862e7
kmk01 <144207120+KMKang01@users.noreply.github.com>
2026-04-11 00:41:48 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        answer = new int [num2 - num1 + 1];
+        int i = 0;
+        while (num1 <= num2) {
+            answer[i++] = numbers[num1++];
+        }
+        return answer;
+    }
+}
