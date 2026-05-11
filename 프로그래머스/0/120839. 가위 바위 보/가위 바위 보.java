e26a4760cac90f8e97909f753fd7b95da0fa1b00
kmk01 <144207120+KMKang01@users.noreply.github.com>
2026-05-12 01:21:19 +0900

    [level 0] Title: 가위 바위 보, Time: 0.26 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(String s : rsp.split("")) {
+            switch(s) {
+                case "0":
+                    sb.append("5");
+                    break;
+                case "2":
+                    sb.append("0");
+                    break;
+                case "5":
+                    sb.append("2");
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+}
