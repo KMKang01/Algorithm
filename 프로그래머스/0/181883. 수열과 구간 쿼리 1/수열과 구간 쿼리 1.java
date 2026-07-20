258933022e241afd835d953dc6bda107c66f0bb4
kmk01 <144207120+KMKang01@users.noreply.github.com>
2026-07-21 00:09:17 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.03 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+//         int[] answer = new int[arr.length];
+//         for(int i = 0 ; i < arr.length; i++) {
+//             answer[i] = arr[i];
+//         }
+        
+        for(int i = 0; i<queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            
+            for(int j = s; j <= e; j++) {
+                arr[j]++;
+            }
+            
+        }
+        return arr;
+    }
+}
