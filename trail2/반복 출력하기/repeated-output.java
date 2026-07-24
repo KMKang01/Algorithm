e2a5f9f1ea57660ded5a0e32c0ae16567e2fe514
kmk01 <144207120+KMKang01@users.noreply.github.com>
2026-07-24 18:11:09 +0900

    [Passed] 반복 출력하기 (131ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<n; i++) {
+            sb.append("12345^&*()_\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
