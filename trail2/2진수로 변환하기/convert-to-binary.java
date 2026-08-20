1c70af6f9fb138141a51c745aa604e24311eebb4
kmk01 <144207120+KMKang01@users.noreply.github.com>
2026-08-21 00:03:05 +0900

    [Passed] 2진수로 변환하기 (123ms, 10MB)

---
+++ trail2/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            sb.append(n%2);
+            n/=2;
+        }
+        if(sb.length() == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(sb.reverse().toString());
+        }
+        
+    }
+}
