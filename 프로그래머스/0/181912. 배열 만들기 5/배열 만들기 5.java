3a20ef2ff36b599d8c8780e79798c1456b81d2ae
kmk01 <144207120+KMKang01@users.noreply.github.com>
2026-08-24 16:59:47 +0900

    [level 0] Title: 배열 만들기 5, Time: 10.85 ms, Memory: 99.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        int [] answer = Arrays.stream(intStrs)
+            .mapToInt(i -> Integer.parseInt(i.substring(s, s+l)))
+            .filter(i -> i > k)
+            .toArray();
+        
+        return answer;
+    }
+}
